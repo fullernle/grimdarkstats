fb2763f0dc67d335d9a860a9afac46df1081387c
lefuller <fullernle@gmail.com>
2021-06-22 13:08:14 -0700

    add axes

--- src/index.js
+++ src/index.js
@@ -13,19 +13,32 @@ const height = +svg.attr("height");
 const render = (data) => {
   const xValue = (d) => d["Avg VP"];
   const yValue = (d) => d.Player;
+  const margin = { top: 20, right: 20, bottom: 20, left: 180 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
   const xScale = d3
     .scaleLinear()
     .domain([0, d3.max(data, xValue)])
-    .range([0, width]);
+    .range([0, innerWidth]);
 
   const yScale = d3
     .scaleBand()
     .domain(data.map(yValue))
-    .range([0, height]);
+    .range([0, innerHeight])
+		.padding(0.1);
 
-  svg
-    .selectAll("rect")
+  // g = group element
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  g.append("g").call(d3.axisLeft(yScale));
+  g.append("g")
+    .call(d3.axisBottom(xScale))
+    .attr("transform", `translate(0, ${innerHeight})`);
+
+  g.selectAll("rect")
     .data(data)
     .enter()
     .append("rect")
