42b16e52b191d1a0e7b601d5dd8fbeb3a956bdb8
lefuller <fullernle@gmail.com>
2021-06-23 11:34:09 -0700

    implement zoomable sunburst

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./styles/index.css";
 import * as d3 from "d3";
+import * as d3plus from 'd3plus';
 import { filterLimit } from "async";
 import regeneratorRuntime from "regenerator-runtime";
 
 const data = d3.csv(
   "https://gist.githubusercontent.com/lefuller/378bb2d512cbbc81ddd66cb0c4a571bf/raw/d3f649c3ba184d62000018a4227cf2905e78bf1a/subfactions.csv",
+  d3.autoType
+);
+
+const playerData = d3.csv(
+  "https://gist.githubusercontent.com/lefuller/378bb2d512cbbc81ddd66cb0c4a571bf/raw/d58f2367dd8c0448e95276e8b5bea5560ab6c57c/Overall.csv",
+  d3.autoType
+);
+
+const factionData = d3.csv(
+  "https://gist.githubusercontent.com/lefuller/378bb2d512cbbc81ddd66cb0c4a571bf/raw/d58f2367dd8c0448e95276e8b5bea5560ab6c57c/Overall.csv",
   d3.autoType
 );
 
@@ -20,26 +31,26 @@ const filterFactions = async (data) => {
     let factionInfo = {
       name: currFaction,
       children: [],
-      colorValue: colorValue,
+      // colorValue: colorValue,
     };
 
     d.forEach((row, i) => {
       if (row.Faction !== currFaction) {
         currFaction = row.Faction;
         filter.children.push(factionInfo);
-        colorValue += 0.025;
+        // colorValue += 0.025;
 
         factionInfo = {
           name: currFaction,
           children: [],
-          colorValue: colorValue,
+          // colorValue: colorValue,
         };
       }
 
       let chapterInfo = {
         name: row.Chapter,
-        size: row["# Lists"],
-        colorValue: colorValue,
+        value: row["# Lists"],
+        // colorValue: colorValue,
       };
       factionInfo.children.push(chapterInfo);
 
@@ -49,7 +60,7 @@ const filterFactions = async (data) => {
   return filter;
 };
 
-const sunBurst = async () => {
+const sunBurst = async (data) => {
   let nodeData = await filterFactions(data);
 
   // Variables
@@ -105,102 +116,172 @@ const sunBurst = async () => {
       return currColor;
     });
 
-		slices.append("svg:title") 
-			.text(d => d.data.name)
-
-  // slices
-  //   .append("text") // <--1
-  //   .filter(function (d) {
-  //     return d.parent;
-  //   }) // <--2
-  //   .attr("transform", function (d) {
-  //     // <--3
-  //     return (
-  //       "translate(" +
-  //       arc.centroid(d) +
-  //       ")rotate(" +
-  //       computeTextRotation(d) +
-  //       ")"
-  //     );
-  //   })
-  //   .attr("dx", "-20") // <--4
-  //   .attr("dy", ".5em") // <--5
-  //   .text(function (d) {
-	// 		console.log(d);
-  //     return d.data.name;
-  //   }); // <--6
+  slices.append("svg:title").text((d) => d.data.name);
 };
 
-function computeTextRotation(d) {
-  var angle = ((d.x0 + d.x1) / Math.PI) * 90; // <-- 1
+// sunBurst(subFactionData);
 
-  // Avoid upside-down labels; labels aligned with slices
-  return angle < 90 || angle > 270 ? angle : angle + 180; // <--2
+const chart = async (nodeData) => {
+  const partition = (data) => {
+    const root = d3
+      .hierarchy(data)
+      .sum((d) => d.value)
+      .sort((a, b) => b.value - a.value);
+    return d3.partition().size([2 * Math.PI, root.height + 1])(root);
+  };
 
-  // Alternate label formatting; labels as spokes
-  //return (angle < 180) ? angle - 90 : angle + 90;  // <-- 3
-}
+  const format = d3.format(",d");
 
-sunBurst();
-// const factions = d3.csv(
-//   "https://gist.githubusercontent.com/lefuller/378bb2d512cbbc81ddd66cb0c4a571bf/raw/884221966feb9a98fcc66b9e960ee11b68cb908e/Overall.csv",
-//   d3.autoType
-// );
+  let data = await filterFactions(nodeData);
+  console.log(data);
+  const width = 932;
+  const radius = width / 6;
+  const root = partition(data);
+  const color = d3.scaleOrdinal(
+    d3.quantize(d3.interpolateRainbow, data.children.length + 1)
+  );
 
-// // const svg = d3.select("svg").attr("width", "1000").attr("height", "500");
+  const arc = d3
+    .arc()
+    .startAngle((d) => d.x0)
+    .endAngle((d) => d.x1)
+    .padAngle((d) => Math.min((d.x1 - d.x0) / 2, 0.005))
+    .padRadius(radius * 1.5)
+    .innerRadius((d) => d.y0 * radius)
+    .outerRadius((d) => Math.max(d.y0 * radius, d.y1 * radius - 1));
 
-// // const width = +svg.attr("width");
-// // const height = +svg.attr("height");
+  root.each((d) => (d.current = d));
 
-// // const renderHrzBar = (data, x, y) => {
-// //   // const xValue = (d) => d["Win %"];
-// //   // const yValue = (d) => d.Faction;
-// //   const xValue = x;
-// //   const yValue = y;
-// //   const margin = { top: 20, right: 20, bottom: 20, left: 180 };
-// //   const innerWidth = width - margin.left - margin.right;
-// //   const innerHeight = height - margin.top - margin.bottom;
+  const svg = d3
+    .select("svg")
+    .attr("width", "1000")
+    .attr("height", "1000")
+    .attr("viewBox", [0, 0, width, width])
+    .style("font", "10px sans-serif");
 
-// //   const xScale = d3
-// //     .scaleLinear()
-// //     .domain([0, d3.max(data, xValue)])
-// //     .range([0, innerWidth]);
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${width / 2},${width / 2})`);
 
-// //   const yScale = d3
-// //     .scaleBand()
-// //     .domain(data.map(yValue))
-// //     .range([0, innerHeight])
-// //     .padding(0.1);
+  const path = g
+    .append("g")
+    .selectAll("path")
+    .data(root.descendants())
+    .join("path")
+    .attr("fill", (d) => {
+      while (d.depth > 1) d = d.parent;
+      return color(d.data.name);
+    })
+    .attr("fill-opacity", (d) =>
+      arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0
+    )
+    .attr("d", (d) => arc(d.current));
 
-// //   const g = svg
-// //     .append("g")
-// //     .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  path
+    .filter((d) => d.children)
+    .style("cursor", "pointer")
+    .on("click", clicked);
 
-// //   g.append("g").call(d3.axisLeft(yScale));
-// //   g.append("g")
-// //     .call(d3.axisBottom(xScale))
-// //     .attr("transform", `translate(0, ${innerHeight})`);
+  // path.append("title").text(
+  //   (d) =>
+  //     `${d
+  //       .ancestors()
+  //       .map((d) => d.data.name)
+  //       .reverse()
+  //       .join("/")}\n${format(d.value)}`
+  // );
+  path.append("title").text((d) => d.data.name);
 
-// //   g.selectAll("rect")
-// //     .data(data)
-// //     .enter()
-// //     .append("rect")
-// //     .attr("fill", ran)
-// //     .attr("y", (d) => yScale(yValue(d)))
-// //     .attr("width", (d) => xScale(xValue(d)))
-// //     .attr("height", yScale.bandwidth());
-// // };
+  const label = g
+    .append("g")
+    .attr("pointer-events", "none")
+    .attr("text-anchor", "middle")
+    .style("user-select", "none")
+    .selectAll("text")
+    .data(root.descendants().slice(1))
+    .join("text")
+    .attr("dy", "0.35em")
+    .attr("fill-opacity", (d) => +labelVisible(d.current))
+    .attr("transform", (d) => labelTransform(d.current))
+    .text((d) => {
+      if (d.data.name) {
+        let name = d.data.name.split(" ");
+        name = name.join("\n");
+				console.log(name);
+        return name;
+      } else {
+				return d.data.name
+			}
+    });
 
-// // subFactions.then((d) => {
-// //   console.log(d);
-// //   renderHrzBar(d, d => d.Faction, d => d.Chapter)
-// // });
+  const parent = g
+    .append("circle")
+    .datum(root)
+    .attr("r", radius)
+    .attr("fill", "none")
+    .attr("pointer-events", "all")
+    .on("click", clicked);
 
-// // factions.then((d) => {
-// //   console.log(d);
-// //   renderHrzBar(
-// //     d,
-// //     (d) => d["Actual Win %"],
-// //     (d) => d.Faction
-// //   );
-// // });
+  function clicked(event, p) {
+    parent.datum(p.parent || root);
+
+    root.each(
+      (d) =>
+        (d.target = {
+          x0:
+            Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) *
+            2 *
+            Math.PI,
+          x1:
+            Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) *
+            2 *
+            Math.PI,
+          y0: Math.max(0, d.y0 - p.depth),
+          y1: Math.max(0, d.y1 - p.depth),
+        })
+    );
+
+    const t = g.transition().duration(750);
+
+    // Transition the data on all arcs, even the ones that aren’t visible,
+    // so that if this transition is interrupted, entering arcs will start
+    // the next transition from the desired position.
+    path
+      .transition(t)
+      .tween("data", (d) => {
+        const i = d3.interpolate(d.current, d.target);
+        return (t) => (d.current = i(t));
+      })
+      .filter(function (d) {
+        return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+      })
+      .attr("fill-opacity", (d) =>
+        arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0
+      )
+      .attrTween("d", (d) => () => arc(d.current));
+
+    label
+      .filter(function (d) {
+        return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+      })
+      .transition(t)
+      .attr("fill-opacity", (d) => +labelVisible(d.target))
+      .attrTween("transform", (d) => () => labelTransform(d.current));
+  }
+
+  function arcVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+  }
+
+  function labelVisible(d) {
+    return d.y1 <= 2.5 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+  }
+
+  function labelTransform(d) {
+    const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
+    const y = ((d.y0 + d.y1) / 2) * radius;
+    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+  }
+};
+
+chart(data);
