d32f3ab3eb28ba7a39c7d544278828b598afb78d
lefuller <fullernle@gmail.com>
2021-06-25 14:44:39 -0700

    style more and add placeholder for personal links

--- src/home.js
+++ src/home.js
@@ -22,51 +22,64 @@ export const drawHome = () => {
   wrapper.append(header);
 
   const text = document.createElement("p");
-  text.classList.add("greeting-text");
   wrapper.append(text);
+	
+	const greetingText = document.createElement("span");
+  greetingText.classList.add("greeting-text");
   const text2 =
     "Grimdark stats seeks to provide visualizations for the data collected over by the guys at ";
 
+	greetingText.append(text2);
   const link = document.createElement("a");
   link.setAttribute("href", "https://www.40kstats.com/");
-  link.append("40kstats");
-  text.append(text2);
-  text.append(link);
+  link.append("40kstats.com");
+	greetingText.append(link);
+	text.append(greetingText);
 
   const section = document.createElement("section");
   section.classList.add("greeting-instructions");
-  const instructions = document.createTextNode("Instructions:");
-  section.append(instructions);
 
-  const sectionText = document.createElement("p");
+	const instrHeader = document.createElement("header");
+	instrHeader.classList.add("instruction-header");
+  const instructions = document.createTextNode("Instructions:");
+	instrHeader.append(instructions);
+  section.append(instrHeader);
+
+  const sectionText = document.createElement("ul");
   sectionText.classList.add("greeting-instructions-text");
-  const lineBreak1 = document.createElement("br");
-  const lineBreak2 = document.createElement("br");
-  const lineBreak3 = document.createElement("br");
-  const lineBreak4 = document.createElement("br");
-  const lineBreak5 = document.createElement("br");
+
+	const li1 = document.createElement("li");
+	const li2 = document.createElement("li");
+	const li3 = document.createElement("li");
+	li3.classList.add("li-header");
+	const li4 = document.createElement("li");
+	const li5 = document.createElement("li");
+	li5.classList.add("li-header");
+	const li6 = document.createElement("li");
+
   const instText1 =
     "Please use the navigation bar to peruse the visualizations";
   const instText2 =
     "Hover over visualizations to gain more detailed information";
-  const instText3 = "Sunburst:";
+  const instText3 = "Most Commonly Played Factions:";
   const instText4 = "Click on center slices to expand breakdown of subfactions";
-  const instText5 = "Percentages:";
+  const instText5 = "Faction Win Rates:";
   const instText6 =
-    "Click on percentages to be taken to a detailed breakdown of each factions wins";
-  sectionText.append(instText1);
-  sectionText.append(lineBreak1);
-  sectionText.append(instText2);
-  sectionText.append(lineBreak2);
-  sectionText.append(instText3);
-  sectionText.append(lineBreak3);
-  sectionText.append(instText4);
-  sectionText.append(lineBreak4);
-  sectionText.append(instText5);
-  sectionText.append(lineBreak5);
-  sectionText.append(instText6);
+    "Click on percentages to be taken to a breakdown of each Faction's wins";
+	li1.append(instText1);
+	li2.append(instText2);
+	li3.append(instText3);
+	li4.append(instText4);
+	li5.append(instText5);
+	li6.append(instText6);
+
+  sectionText.append(li1);
+  sectionText.append(li2);
+  sectionText.append(li3);
+  sectionText.append(li4);
+  sectionText.append(li5);
+  sectionText.append(li6);
 
   section.append(sectionText);
-
   wrapper.append(section);
 };
